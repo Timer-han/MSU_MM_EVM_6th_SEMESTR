37ba9b6621813a0c95d09205e0bd0f0d1205f594
Timer-han <tpetrunnikov@mail.ru>
2025-06-05 18:53:11 +0300

    vault backup: 2025-06-05 18:53:11

--- Homeworks/1D/window.cpp
+++ Homeworks/1D/window.cpp
@@ -131,9 +131,9 @@ void fill_newton_coef_calc (
     }
   // printf("\n");
 
-  for (int i = 1; i < 2 * n + 1; i++)
+  for (int i = 0; i < 2 * n; i++)
     {
-      for (int j = 2 * n + 1; j >= i; j--)
+      for (int j = 2 * n; j >= i; j--)
         {
           result[j + 1] = (result[j + 1] - result[j]) / (x[j + 1] - x[j - i]);
           // printf("c[%d]=%8.3e | ", j + 1, result[j + 1]);
