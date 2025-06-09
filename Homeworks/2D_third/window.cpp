1965aaa1902c05b94c2eaf95d1bc55eac939eaea
Timer-han <tpetrunnikov@mail.ru>
2025-06-09 08:16:54 +0300

    vault backup: 2025-06-09 08:16:54

--- Homeworks/2D_third/window.cpp
+++ Homeworks/2D_third/window.cpp
@@ -6,49 +6,41 @@
 
 #define L2G(X, Y) (l2g((X), (Y), y_min, y_max))
 
-static
 double f_0(double /* x */, double /* y */)
 {
 	return 1;
 }
 
-static
 double f_1(double x, double /* y */)
 {
 	return x;
 }
 
-static
 double f_2(double /* x */, double y)
 {
 	return y;
 }
 
-static
 double f_3(double x, double y)
 {
 	return x + y;
 }
 
-static
 double f_4(double x, double y)
 {
 	return sqrt(x * x + y * y);
 }
 
-static
 double f_5(double x, double y)
 {
 	return x * x + y * y;
 }
 
-static
 double f_6(double x, double y)
 {
 	return exp(x * x - y * y);
 }
 
-static
 double f_7(double x, double y)
 {
 	return 1. / (25 *(x * x + y * y) + 1);
