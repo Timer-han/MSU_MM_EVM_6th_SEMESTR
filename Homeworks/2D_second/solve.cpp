396e0afe6305166943198aa6f567c506be75ee73
Timer-han <tpetrunnikov@mail.ru>
2025-06-09 03:17:52 +0300

    vault backup: 2025-06-09 03:17:52

--- Homeworks/2D_second/solve.cpp
+++ Homeworks/2D_second/solve.cpp
@@ -4,9 +4,49 @@
 #include "thread_work.h"
 #include "solve.h"
 
-#define EPS (1e-4)
+#define EPS (1e-10)
 
 static double *results = nullptr;
+
+double f_0 (double /*x*/, double /*y*/)
+{
+  return 1;
+}
+
+double f_1 (double x, double /*y*/)
+{
+  return x;
+}
+
+double f_2 (double /*x*/, double y)
+{
+  return y;
+}
+
+double f_3 (double x, double y)
+{
+  return x + y;
+}
+
+double f_4 (double x, double y)
+{
+  return sqrt (x * x + y * y);
+}
+
+double f_5 (double x, double y)
+{
+  return x * x + y * y;
+}
+
+double f_6 (double x, double y)
+{
+  return exp (x * x - y * y);
+}
+
+double f_7 (double x, double y)
+{
+  return 1 / ((x * x + y * y) * 25 + 1);
+}
 
 void thread_rows (int n, int p, int k, int &i1, int &i2)
 {
